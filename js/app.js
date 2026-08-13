c37bffba7218085fcd4b4d580b96a5bdd2b7a1e1
olgamontanarii <olga.montanari@live.u-tad.com>
2026-08-13 02:29:20 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 console.log("✅ app.js cargado correctamente");
 console.log("Días disponibles:", tripDays);
+
 /* =========================================================
    APLICACIÓN DEL ITINERARIO
 
