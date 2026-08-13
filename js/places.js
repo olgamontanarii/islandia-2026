4375758e21ad5e9a7a83ddb3f4c462494812a350
olgamontanarii <olga.montanari@live.u-tad.com>
2026-08-13 03:02:20 +0200

    Create places.js

---
+++ js/places.js
@@ -0,0 +1,67 @@
+/* =========================================================
+   PUNTOS ÚTILES DEL VIAJE
+
+   Aquí guardaremos:
+   - campings
+   - supermercados
+
+   Más adelante también podremos añadir:
+   - gasolineras
+   - piscinas
+   - duchas
+   - talleres
+========================================================= */
+
+
+/* =========================================================
+   CAMPINGS
+========================================================= */
+
+const campsites = [
+
+  {
+    name: "Hella Camping",
+
+    location: {
+      lat: 63.8350,
+      lng: -20.4000
+    },
+
+    description:
+      "Camping útil para dormir tras el Círculo Dorado.",
+
+    tags: [
+      "🚿 Duchas",
+      "🚐 Camper",
+      "📍 Hella"
+    ]
+  }
+
+];
+
+
+/* =========================================================
+   SUPERMERCADOS
+========================================================= */
+
+const supermarkets = [
+
+  {
+    name: "Bónus Selfoss",
+
+    location: {
+      lat: 63.9330,
+      lng: -20.9970
+    },
+
+    description:
+      "Supermercado económico y práctico para comprar comida.",
+
+    tags: [
+      "🛒 Compra grande",
+      "💰 Económico",
+      "📍 Selfoss"
+    ]
+  }
+
+];
